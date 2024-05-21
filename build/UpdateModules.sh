88d331d2b0dccb8019574eeabafe8135cd0811fc
Jacksonnnnn <jhuse01@gmail.com>
2024-05-21 15:52:35 -0400

    Update dependency bash files

--- build/UpdateModules.sh
+++ build/UpdateModules.sh
@@ -1,3 +1,5 @@
+python3 -m pip install --upgrade pip
+
 python3 -m pip install --upgrade scipy
 python3 -m pip install --upgrade networkx
 python3 -m pip install --upgrade matplotlib
@@ -6,7 +8,4 @@ python3 -m pip install --upgrade sympy
 python3 -m pip install --upgrade Pillow
 python3 -m pip install --upgrade mpmath
 python3 -m pip install --upgrade schemdraw==0.17
-python3 -m pip install --upgrade pycairo
 python3 -m pip install --upgrade pipwin
-python3 -m pip install svglib
-python3 -m pip install tksvg
--- build/createWinDist.sh
+++ build/createWinDist.sh
@@ -11,6 +11,7 @@ curl -o "dist/assets/taylor.png" https://gyazo.com/a0c666e23c31024e4b5097f626d55
 curl -o "dist/assets/rearranged.png" https://gyazo.com/f5846f1ed0e124ad66cb5690437a8cd8.png
 
 python3 -m pip install --upgrade pip
+
 python3 -m pip install --upgrade scipy
 python3 -m pip install --upgrade networkx
 python3 -m pip install --upgrade matplotlib
@@ -19,11 +20,8 @@ python3 -m pip install --upgrade sympy
 python3 -m pip install --upgrade Pillow
 python3 -m pip install --upgrade mpmath
 python3 -m pip install --upgrade schemdraw==0.17
-python3 -m pip install --upgrade pycairo
-python3 -m pip install --upgrade cairocffi
-python3 -m pip install --upgrade cairosvg
-python3 -m pip install --upgrade pyinstaller
-python3 -m pip uninstall pathlib
+python3 -m pip install --upgrade pipwin
+python3 -m pip install pathlib
 
 pyinstaller gui.py Function.py FuncTypes.py GateTypes.py NotGateTypes.py RearrangeType.py Util.py --name "FUNSC --debug" --clean --onefile --icon "assets/frame0/UK logo-white.png" --add-data "assets/frame0/*.png;assets/frame0" --hidden-import=scipy --hidden-import=networkx --hidden-import=matplotlib --hidden-import=PIL --hidden-import=PIL._imagingtk --hidden-import=PIL._tkinter_finder --hidden-import=sympy --hidden-import=mpmath --hidden-import=tk --hidden-import=schemdraw --debug=imports
 pyinstaller gui.py Function.py FuncTypes.py GateTypes.py NotGateTypes.py RearrangeType.py Util.py --name "FUNSC" --clean -w --onefile --icon "assets/frame0/UK logo-web.png" --splash "assets/frame0/UK ECE mark.png" --add-data "assets/frame0/*.png;assets/frame0" --hidden-import=scipy --hidden-import=networkx --hidden-import=matplotlib --hidden-import=PIL --hidden-import=PIL._imagingtk --hidden-import=PIL._tkinter_finder --hidden-import=sympy --hidden-import=mpmath --hidden-import=tk --hidden-import=schemdraw --debug=imports
